c7912bdba99780f56480b298b44a3e9ecdf0d259
정준영 <jyjeong620@naver.com>
2020-10-15 15:53:58 +0900

    @Configuration 를 이용한 singleton 보장 방법 테스트

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,31 +10,35 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 실제 동작에 필요한 구현 객체를 생성한다.
  */
-@Configurable
+@Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy(){
-//        return new FixDiscountPolicy();   //구현체를 바꿀때는 이런식으로 AppConfig에서 처리해주면 된다
+//        return new FixDiscountPolicy();   //구현체를 바꿀때는 이런식으로 AppConfig 에서 처리해주면 된다
         return new RateDiscountPolicy();
     }
 
