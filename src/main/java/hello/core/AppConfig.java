b3b5f6fa0d4be23907e7f5817aa4c69460f97fc2
정준영 <jyjeong620@naver.com>
2020-10-15 16:31:54 +0900

    git Test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+
     @Bean
     public MemberService memberService(){
         System.out.println("call AppConfig.memberService");
