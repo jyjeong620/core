95fbd8bf84edf145b3526028d64bfcbe0b94c283
정준영 <jyjeong620@naver.com>
2020-10-16 15:25:31 +0900

    Filter를 이용한 Component 적용 미적용 테스트, 새로운 annotation 생성

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",        //시작하는 package 위치 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class) //AppConfig.class 를 제외하기위해
 )
 public class AutoAppConfig {
