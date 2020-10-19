9fcfbe74a32e18610ecc007acdc87b5c0ec95ffa
정준영 <jyjeong620@naver.com>
2020-10-19 16:53:32 +0900

    Lombok을 사용하여 코드 간결화 @Getter, @Setter, @RequiredArgsConstructor 등

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,22 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final 이 붙은 필드를 모아서 생성자를 자동으로 만들어줌(밑에 주석처리한부분)
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; //= new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy; // new FixDiscountPolicy(); // 이렇게하면 인터페이스 뿐만아니라 구현체도 같이 의존하게 된다.
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired  //생성자가 한개면 생략가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
